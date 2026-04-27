faf55679a5898e4cd7f490c4d79ac8f2cfde4547
sunshine-web-coder <bukkyjames11290@gmail.com>
2026-04-27 20:04:37 +0100

    mdj77

--- lib/mockData.ts
+++ lib/mockData.ts
@@ -139,10 +139,8 @@ export const users: User[] = [
     firstName: 'JACOB',
     lastName: 'GABRIELLE',
     email: 'chri**ntz8@gmail.com',
-    username: 'l',
-    password: 'l',
-    // username: 'Christophkantz8',
-    // password: 'Am@12345',
+    username: 'Christophkantz8',
+    password: 'Am@12345',
     transactionCode: '7894',
     createdAt: '2026-4-27', // y-m-d
     transactionMsg:
