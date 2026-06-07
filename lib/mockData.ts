4e3053730fef864edbf2f9a7e0ea46cbc00251fe
bukkyjames11290 <bukkyjames11290@gmail.com>
2026-06-07 19:15:05 +0100

    refed

--- lib/mockData.ts
+++ lib/mockData.ts
@@ -143,6 +143,7 @@ export const users: User[] = [
     password: 'Am@12345',
     transactionCode: '7894',
     createdAt: '2026-4-27', // y-m-d
+    updatedAt: '2026-6-7', // y-m-d
     transactionMsg:
       'Your account is on hold. You cannot make transactions right now. Please contact our customer service team on scrtbrclskinternet@gmail.com for assistance.',
     accounts: [
@@ -156,7 +157,7 @@ export const users: User[] = [
            {
             merchant: 'Wire Transfer to Christoph Kantz',
             category: '****6800',
-            date: 'Apr 26, 2026',
+            date: 'June 6, 2026',
             amount: 20000000,
             status: 'success'
           },
